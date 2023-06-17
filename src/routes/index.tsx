46a2d746604aa2c7b44ead607a6ddeb487240687
Brenley Dueck <brenleydueck@gmail.com>
2023-06-17 08:21:19 -0500

    Add resource, and suspense

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,69 +1,73 @@
-import { Show, createMemo, createSignal } from "solid-js";
+import { ErrorBoundary, Show, Suspense, createEffect, createMemo, createResource, createSignal } from "solid-js";
 
 const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
 export default function App() {
-  const [cityName, setCityName] = createSignal('');
-  const [submittedCityName, setSubittedCityName] = createSignal('');
-  const [tempKelvin, setTempKelvin] = createSignal<number>();
-  const [error, setError] = createSignal(false);
-  const [tempUnit, setTempUnit] = createSignal('Celcius')
+  const [cityName, setCityName] = createSignal<string>();
+  const [tempUnit, setTempUnit] = createSignal('Celcius');
 
-  const tempCelsius = createMemo(() => {
-    const kelvinVal = tempKelvin();
-    return kelvinVal ? Math.round(kelvinVal - 273.15) : undefined;
-  });
-
-  const handleSubmit = async (e: SubmitEvent) => {
-    e.preventDefault();
-
-    if (!cityName()) return;
+  const [temperature] = createResource(cityName, async (city) => {
 
     const result = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName()}&appid=${apiKey}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
     );
 
     const weather = await result.json();
 
-    setError(!weather?.main?.temp);
-    setTempKelvin(weather?.main?.temp);
-    setSubittedCityName(weather?.name);
+    if (weather.cod !== 200) {
+      throw new Error(weather.message);
+    }
+
+    return {
+      kelvin: weather.main.temp,
+      celcius: Math.round(weather.main.temp - 273.15)
+    }
+  });
+
+  const handleSubmit = async (e: Event) => {
+    e.preventDefault();
+
+    const form = e.currentTarget as HTMLFormElement;
+    const formElements = form.elements as typeof form.elements & {
+      cityName: HTMLInputElement;
+    };
+    const cityName = formElements.cityName.value;
+
+    if (!cityName) return;
+    setCityName(cityName);
   };
 
   return (
     <>
       <h2>Weather</h2>
       <form onSubmit={handleSubmit}>
-        <input value={cityName()} placeholder="City name" onInput={(e) => {
-          setCityName(e.target.value);
-        }} />
+        <input name="cityName" placeholder="City name" />
         <button type="submit">
           Check Weather
         </button>
       </form>
-      <Show when={error()}>
-        <p>
-          <strong>An error occured</strong>
-        </p>
-      </Show>
-      <Show when={submittedCityName()}>
-        <p>
-          The current weather in <strong>{submittedCityName()}</strong> is:{' '}
+      <ErrorBoundary fallback={<p>Something went wrong</p>}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Show when={temperature()}>
+            <p>
+              The current weather in <strong>{cityName()}</strong> is:{' '}
 
-          <Show when={tempUnit() === 'Celcius'} fallback={tempKelvin()}>
-            {tempCelsius()}
+              <Show when={tempUnit() === 'Celcius'} fallback={temperature()?.kelvin}>
+                {temperature()?.celcius}
+              </Show>
+            </p>
+            <p>
+
+              <button onClick={() => {
+                setTempUnit('Celcius');
+              }}>Celcius</button>
+
+              <button onClick={() => {
+                setTempUnit('Kelvin');
+              }}>Kelvin</button></p>
           </Show>
-        </p>
-        <p>
-
-          <button onClick={() => {
-            setTempUnit('Celcius');
-          }}>Celcius</button>
-
-          <button onClick={() => {
-            setTempUnit('Kelvin');
-          }}>Kelvin</button></p>
-      </Show>
+        </Suspense>
+      </ErrorBoundary >
     </>
   )
 }
