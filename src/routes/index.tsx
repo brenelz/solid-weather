b3316dd8c97f56e4d3543c9954e9cdc393d01620
Brenley Dueck <brenleydueck@gmail.com>
2023-06-16 22:23:12 -0500

    Make simple weather app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,66 @@
+import { Show, createSignal } from "solid-js";
+
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+
 export default function App() {
+  const [cityName, setCityName] = createSignal('');
+  const [submittedCityName, setSubittedCityName] = createSignal('');
+  const [tempKelvin, setTempKelvin] = createSignal<number>();
+  const [error, setError] = createSignal(false);
+  const [tempUnit, setTempUnit] = createSignal('Celcius')
+
+  const tempCelsius = () => tempKelvin() ? Math.round(tempKelvin() - 273.15) : undefined;
+
+  const handleSubmit = async (e: SubmitEvent) => {
+    e.preventDefault();
+
+    if (!cityName()) return;
+
+    const result = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName()}&appid=${apiKey}`
+    );
+
+    const weather = await result.json();
+
+    setError(!weather?.main?.temp);
+    setTempKelvin(weather?.main?.temp);
+    setSubittedCityName(weather?.name);
+  };
+
   return (
     <>
-      Initial
+      <h2>Weather</h2>
+      <form onSubmit={handleSubmit}>
+        <input value={cityName()} placeholder="City name" onInput={(e) => {
+          setCityName(e.target.value);
+        }} />
+        <button type="submit">
+          Check Weather
+        </button>
+      </form>
+      <Show when={error()}>
+        <p>
+          <strong>An error occured</strong>
+        </p>
+      </Show>
+      <Show when={submittedCityName()}>
+        <p>
+          The current weather in <strong>{submittedCityName()}</strong> is:{' '}
+
+          <Show when={tempUnit() === 'Celcius'} fallback={tempKelvin()}>
+            {tempCelsius()}
+          </Show>
+        </p>
+        <p>
+
+          <button onClick={() => {
+            setTempUnit('Celcius');
+          }}>Celcius</button>
+
+          <button onClick={() => {
+            setTempUnit('Kelvin');
+          }}>Kelvin</button></p>
+      </Show>
     </>
   )
 }
